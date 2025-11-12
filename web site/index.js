e2366102eb698d063fcdd53bf2a752f6551e190b
k1nosh1mo <grigorkincom@gmail.com>
2025-11-12 10:42:40 +0500

    Delete web site directory

--- web site/index.js
+++
@@ -1,6 +0,0 @@
-let btn=document.querySelector(".my_btn");
-btn.addEventListener("click",click_btn);
-function click_btn(){
-   document.querySelector("body").classList.toggle("new_class");
-   btn.classList.toggle("new_class")
-}
